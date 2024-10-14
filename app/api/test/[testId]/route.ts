05d1e14c295af587312eeee6c7ed8ed43c197e44
Atharv Vibhute <atharv.av.codes@gmail.com>
2024-10-14 12:39:14 +0530

    added Hono and server side route protection

---
+++ app/api/test/[testId]/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = (
+  request: NextRequest,
+  { params }: { params: { testId: string } }
+) => {
+  return NextResponse.json({
+    message: "Hello world!",
+    testId: params.testId,
+  });
+};
